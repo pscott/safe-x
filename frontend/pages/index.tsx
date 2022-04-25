39c29fa628c123aa89c73017b30db80ab548e7d4
Green <40367733+greenlucid@users.noreply.github.com>
2022-04-25 23:07:12 +0200

    chore: hello

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,14 @@
+import { useStarknet } from "@starknet-react/core"
 import Link from "next/link"
 
 const Home: React.FC = () => {
+  const {account} = useStarknet()
   return (
     <div>
       <h1>Welcome to safe-x</h1>
+      {account === undefined
+        ? <p>Please connect your Argent wallet</p>
+        : <p>Hello {account}</p>}
       <Link href={"space-factory"}>
         <a className="createSpaceLink">Create a space</a>
       </Link>
