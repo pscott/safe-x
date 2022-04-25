1793d4219828dc8c3f66899686a0df8a0ed9fb95
Green <40367733+greenlucid@users.noreply.github.com>
2022-04-25 22:59:48 +0200

    feat: connect to argent

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,12 +3,41 @@ import { Provider } from "react-redux"
 import { configureStore } from "@reduxjs/toolkit"
 import slice from "../src/slice"
 
+import {
+  InjectedConnector,
+  StarknetProvider,
+  useStarknet,
+} from "@starknet-react/core"
+
 const store = configureStore({ reducer: slice.reducer })
+
+const ConnectToWallet = () => {
+  const { connect, connectors, account } = useStarknet()
+  return (
+    <div>
+      {account === undefined &&
+        connectors.map((connector) =>
+          connector.available() ? (
+            <button key={connector.id} onClick={() => connect(connector)}>
+              Connect {connector.name}
+            </button>
+          ) : null
+        )}
+    </div>
+  )
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <StarknetProvider
+        connectors={[new InjectedConnector({ showModal: true })]}
+      >
+        <>
+          <ConnectToWallet />
+          <Component {...pageProps} />
+        </>
+      </StarknetProvider>
     </Provider>
   )
 }
